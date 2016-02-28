00a7b394b7c7b5bf0d5db9d80fa883c37de3e765
marincovic <covic.marin1@gmail.com>
2016-02-28 16:15:02 -0500

    V1.5.2
    Ispočetka počeo pisati GraphNode

--- Structures/GraphNode.h
+++ Structures/GraphNode.h
@@ -1,156 +1,47 @@
 #pragma once
 
 #include<vector>
-#include<exception>
-#include<algorithm>
 
-
-// Nije mi jasno èemu služi èlan moveWeightType: ne koristite ga nigdje,
-// osim u metodi GetWieghtOfMove (koja je krivo nazvana, treba biti GetWeightOfMove)
-// za koju nemate niti jedan test napisan! Njome nepotrebno zagaðujete suèelje!
-// Nekako mi se èini da je NextChildStruct nepotrebna klasa jer ju ionako
-// svugdje koristite kao ovojnicu oko pokazivaèan na sljedeæi GraphNode
-
-// VAŽNO: smatram da biste sustavno trebali koristiti kljuènu rijeè class 
-// umjesto struct. Struct je praktièki atavizam iz jezika C, zadržan zbog
-// kompatibilnosti sa starim kodom, a danas se koristi samo za jednostavne 
-// strukture koje sve èlanove imaju javno dostupne, da se ne mora eksplicitno
-// pisati javno pravo pristupa
-template<typename GraphNode, typename moveWeightType>
-struct NextChildStruct
+template<typename typeOfDataMember>
+class GraphNode
 {
 public:
-	// Prazni konstruktor nema smisla jer prazni child je u biti nullptr
-	//NextChildStruct() {}
-	NextChildStruct(const GraphNode& nextNode, moveWeightType weightOfMove = 0) : m_NextChild(nextNode), m_weightOfMove(weightOfMove) {}
-	~NextChildStruct()
+	GraphNode() {};
+	GraphNode(typeOfDataMember dataMember) : m_dataMember(dataMember) {};
+	~GraphNode() { m_vectorOfAllChildren.clear(); }
+
+	const typeOfDataMember& GetDataOfMember() { return m_dataMember; }
+
+	void AddMember(const typeOfDataMember& dataOfNewMember)
 	{
-		// ovo ne valja jer pozivate operator delete na objektu, a ne na pokazivaèu.
-		// Izvoðenje donje naredbe æe srušiti program (baciti iznimku)
-		delete m_NextChild;
-		// osim što u ovom sluèaju ovo nema smisla, èak i kada bi m_NextChild bio pokazivaè, nema
-		// potrebe ovo raditi jer to radite u destruktoru i nakon toga m_NextChild neæe
-		// više postojati (kao da ste pucali u èovjeka koji upravo umire i koji bi ionako 
-		// sljedeæeg trenutka bio klinièki mrtav)
-		m_NextChild = nullptr;
+		m_vectorOfAllChildren.push_back(new GraphNode<typeOfDataMember>(dataOfNewMember));
 	}
 
-	void SetNextChild(const GraphNode& nextChild) { m_NextChild = nextChild; }
-	const GraphNode& GetNextChild() { return m_NextChild; }
+	void RemoveMember(const typeOfDataMember& dataOfMemberToBeDeleted) {
+		if (!NumberOfChildren()) throw std::out_of_range("Node has no children");
 
-	void SetWeightOfMove(moveWeightType weightOfMove) { m_weightOfMove = weightOfMove; }
-	int GetWieghtOfMove() { return m_weightOfMove; }
+		if (!ContainsMember(dataOfMemberToBeDeleted)) throw std::out_of_range("Node has no child with contaning that data");
 
-private:
-	GraphNode m_NextChild;
-	moveWeightType m_weightOfMove;
-};
-
-template<typename dataTypeOfNode, typename moveWeightType>
-struct GraphNode
-{
-public:
-	GraphNode() {}
-	GraphNode(const dataTypeOfNode& dataOfNode) :m_dataOfNode(dataOfNode) {}
-	GraphNode(const dataTypeOfNode& dataOfNode, 
-		const dataTypeOfNode& dataOfFirstChildNode,
-		const moveWeightType& weightOfMovemenToNewChild = 0)
-	{
-		m_dataOfNode = dataOfNode;
-		m_vectorOfNodeChildren.push_back(new NextChildStruct<GraphNode<dataTypeOfNode, moveWeightType>*, moveWeightType>(new GraphNode<dataTypeOfNode, moveWeightType>(dataOfFirstChildNode,weightOfMovemenToNewChild)));
-	}
-	
-	GraphNode(const dataTypeOfNode& dataOfNode, 
-		const GraphNode<dataTypeOfNode, moveWeightType>* pToChild,
-		const moveWeightType& weightOfMovemenToNewChild = 0)
-	{
-		m_dataOfNode = dataOfNode;
-		m_vectorOfNodeChildren.push_back(new NextChildStruct<GraphNode<dataTypeOfNode, moveWeightType>*, moveWeightType>(new GraphNode<dataTypeOfNode, moveWeightType>(pToChild, weightOfMovemenToNewChild)));
-	}
-	
-	virtual ~GraphNode()
-	{
-		m_vectorOfNodeChildren.clear();
-	}
-
-	const dataTypeOfNode& GetDataOfNode() { return m_dataOfNode; }
-	void SetDataOFNode(const dataTypeOfNode& newDataofNode) { m_dataOfNode = newDataofNode; }
-
-	// Promijenio sam tip indeksa u size_t. 
-	// Na taj naèin ne treba provjeravati je li indeks sluèajno manji od 0,
-	// niti kompajler javlja upozorenje
-	GraphNode<dataTypeOfNode,moveWeightType>* GetChildAtIndex(size_t index)
-	{
-		if (!m_vectorOfNodeChildren.size())
-			return nullptr;
-		if (index > m_vectorOfNodeChildren.size())
-			throw std::out_of_range("Index out of range");
-		return m_vectorOfNodeChildren.at(index)->GetNextChild();
-	}
-	
-	const moveWeightType& GetMovementWeightToChildAtIndex(int index) {};
-	
-	void AddNewChild(GraphNode<dataTypeOfNode, moveWeightType>* newChildToBeAdded, moveWeightType WeightOFMovement = 0)
-	{
-		m_vectorOfNodeChildren.push_back(new NextChildStruct<GraphNode<dataTypeOfNode, moveWeightType>*, moveWeightType>(newChildToBeAdded,WeightOFMovement));
-	}
-	
-	void AddNewChild(const dataTypeOfNode& dataOfNewChild,moveWeightType WeightOFMovement = 0) {
-		GraphNode<dataTypeOfNode, moveWeightType>* tempNode = new GraphNode<dataTypeOfNode, moveWeightType>(dataOfNewChild);
-		m_vectorOfNodeChildren.push_back(new NextChildStruct<GraphNode<dataTypeOfNode,moveWeightType>*, moveWeightType>(tempNode,WeightOFMovement));
-	}
-
-	// ovaj èlan otkriva internu implementaciju bilo kome tko vidi GraphNode,
-	// što nije dobro. Ja bih ovo napravio privatnim èlanom, a ako je potrebno,
-	// klasu koja koristi GraphNode bih proglasio prijateljem, tako da može koristiti 
-	// ovu metodu ako treba
-	void RemoveChild(GraphNode<dataTypeOfNode, moveWeightType>* childToBeRemoved)
-	{
-		for (auto it = m_vectorOfNodeChildren.begin(); it != m_vectorOfNodeChildren.end(); ++it)
+		for (auto it = m_vectorOfAllChildren.begin(); it != m_vectorOfAllChildren.end(); ++it)
 		{
-			if (childToBeRemoved == (*it)->GetNextChild())
-			{
-				m_vectorOfNodeChildren.erase(it);
-				return;
-			}
+			if ((*it)->GetDataOfMember() == dataOfMemberToBeDeleted)
+				it = m_vectorOfAllChildren.erase(it);
 		}
-		throw std::out_of_range("Child does not exist");
-	}
-	
-	void RemoveChild(const dataTypeOfNode& dataOfChildToBeRemoved)
-	{
-		for (auto it = m_vectorOfNodeChildren.begin(); it != m_vectorOfNodeChildren.end(); ++it)
-		{
-			if (dataOfChildToBeRemoved == (*it)->GetNextChild()->GetDataOfNode())
-			{
-				m_vectorOfNodeChildren.erase(it);
-				return;
-			}
-		}
-		throw std::out_of_range("Child does not exist");
 	}
 
-	bool ContainsChild(GraphNode<dataTypeOfNode, moveWeightType>* childToBeFound)
-	{
-		for (auto it = m_vectorOfNodeChildren.begin(); it != m_vectorOfNodeChildren.end(); ++it)
+	bool ContainsMember(const typeOfDataMember& dataOfMemberToBeSearchedFor) {
+		if (!NumberOfChildren()) return false;
+		for (auto it = m_vectorOfAllChildren.begin(); it != m_vectorOfAllChildren.end(); ++it)
 		{
-			if ((*it)->GetNextChild() == childToBeFound)
-				return true;
-		}
-		return false;
-	}
-	
-	bool ContainsChild(dataTypeOfNode dataOfChildToBeFound)
-	{
-		for (auto it = m_vectorOfNodeChildren.begin(); it != m_vectorOfNodeChildren.end(); ++it)
-		{
-			if ((*it)->GetNextChild()->GetDataOfNode() == dataOfChildToBeFound)
+			if ((*it)->GetDataOfMember() == dataOfMemberToBeSearchedFor)
 				return true;
 		}
 		return false;
 	}
 
+	int NumberOfChildren() { return m_vectorOfAllChildren.size(); }
+
 private:
-	dataTypeOfNode m_dataOfNode;
-	std::vector<NextChildStruct<GraphNode<dataTypeOfNode,moveWeightType>*,moveWeightType>*> m_vectorOfNodeChildren;
+	typeOfDataMember m_dataMember;
+	std::vector<GraphNode<typeOfDataMember>*> m_vectorOfAllChildren;
 };
--- UnitTests/GraphTest.cpp
+++ UnitTests/GraphTest.cpp
@@ -5,193 +5,63 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTests
 {
-	TEST_CLASS(GraphNodeTest)
+	TEST_CLASS(GraphNode2Test)
 	{
-	public: 
-		TEST_METHOD(GraphNode_GetDataReturnsDataOFNode)
+	public:
+		TEST_METHOD(GraphNode_SizeReturns0ForNodeWithNoChildren)
 		{
-			GraphNode<int, int> node(5);
-			Assert::AreEqual(5, node.GetDataOfNode());
+			GraphNode<int> node;
+			Assert::AreEqual(0, node.NumberOfChildren());
+		}
+		TEST_METHOD(GraphNode_AddMemberAddsNewMemberToTheList) {
+			GraphNode<int> node(5);
+			node.AddMember(10);
+			Assert::AreEqual(1, node.NumberOfChildren());
 		}
 
-		// Ima li smisla inicijalizirati èvor koji nema vrijednost? 
-		// Po meni to nema smisla jer èvor služi da sadrži neku vrijednost.
-		// Zato mislim da prazan konstruktor ne bi smio postojati i ovaj test 
-		// nema smisla. 
-		TEST_METHOD(GraphNode_SetDataOfNodeSetsDataOfNodeToWantedValue)
+		TEST_METHOD(GraphNode_GetDataMemberReturnsDataOfMember)
 		{
-			GraphNode<int, int> node;
-			node.SetDataOFNode(5);
-			Assert::AreEqual(5, node.GetDataOfNode());
+			GraphNode<int> node(9);
+			Assert::AreEqual(9, node.GetDataOfMember());
 		}
 
-		TEST_METHOD(GraphNode_GetChildAtIndexsReturnsNullptrWhenGraphHasNoChildren)
+		TEST_METHOD(GraphNode_ContainsReturnsTrueIfMemberDoesContainChild)
 		{
-			GraphNode<int, int> node(5);
-			// donja naredba ne valja:
-			// 1. u što god nullptr kastali, to æe i dalje biti nullptr. Stoga je donje kastanje besmisleno!
-			//    Assert::AreEqual(static_cast<GraphNode<int, int>*>(nullptr), node.GetChildAtIndex(0));
-			// 2. Da biste mogli napisati test u ovom obliku:
-			//    Assert::AreEqual(nullptr, node.GetChildAtIndex(0));
-			//    morate napisati preoptereæenu verziju metode Assert::AreEqual() koja æe usporeðivati
-			//    objekte korisnièkog tipa GraphNode<,>* . Buduæi da to (nadam se) ne želite raditi,
-			//    provjeru jednostavno napišite na sljedeæi naèin:
-			Assert::IsTrue(node.GetChildAtIndex(0) == nullptr);
+			GraphNode<int> node(5);
+			node.AddMember(10);
+			node.AddMember(15);
+			Assert::IsTrue(node.ContainsMember(10));
+		}
+		TEST_METHOD(GraphNode_ContainsMemberReturnsFalseIfMemberDoesContainChild)
+		{
+			GraphNode<int> node(5);
+			node.AddMember(10);
+			node.AddMember(15);
+			Assert::IsFalse(node.ContainsMember(6));
 		}
 
-		TEST_METHOD(GraphNode_GetChildAtIndexThrowsExceptionWhenIndexIsOutOfRange)
+		TEST_METHOD(GraphNode_RemoveRemovesChildFromList)
 		{
-			GraphNode<int, int> node(5, 6);
+			GraphNode<int> node(5);
+			node.AddMember(10);
+			node.AddMember(15);
+			node.RemoveMember(10);
+			Assert::IsFalse(node.ContainsMember(10));
+			Assert::AreEqual(1, node.NumberOfChildren());
+		}
+		TEST_METHOD(GraphNode_RemoveMemberThrowExceptionIfNodeDoesNotContainChild)
+		{
+			GraphNode<int> node(5);
+			node.AddMember(10);
+			node.AddMember(15);
 			try {
-				node.GetChildAtIndex(2);
+				node.RemoveMember(9);
 				Assert::Fail();
 			}
 			catch (std::out_of_range&)
 			{
 				Assert::IsTrue(true);
 			}
-		}
-
-		TEST_METHOD(GraphNode_GetChildAtIndexReturnsNextChildInList)
-		{
-			GraphNode<int, int> node(5, 6);
-			Assert::AreEqual(6, node.GetChildAtIndex(0)->GetDataOfNode());
-		}
-	
-		TEST_METHOD(GraphNode_AddNewChildAddsNewChildToListWhenWeightIs0)
-		{
-			GraphNode<int, int> node(5);
-			node.AddNewChild(6);
-			Assert::AreEqual(6, node.GetChildAtIndex(0)->GetDataOfNode());
-		}
-
-		TEST_METHOD(GraphNode_AddNewChildAddsNewChildViaPointerWhenWeightIs0)
-		{
-			GraphNode<int, int> node(5);
-			node.AddNewChild(new GraphNode<int, int>(6));
-			Assert::AreEqual(6, node.GetChildAtIndex(0)->GetDataOfNode());
-		}
-	
-		// test pada jer GetChildAtIndex(0) vraæa nullptr i ne bace iznimku
-		TEST_METHOD(GraphNode_RemoveChildThrowExceptionIfNodeHasNoChildren)
-		{
-			try {
-				GraphNode<int, int> node;
-				node.GetChildAtIndex(0);
-				Assert::Fail();
-			}
-			catch (std::out_of_range&)
-			{
-				Assert::IsTrue(true);
-			}
-		}
-	
-		// test pada jer GetChildAtIndex(2) vraæa nullptr i ne bace iznimku
-		TEST_METHOD(GraphNode_RemoveChildThrowsExceptionIfIdexOutOfRange)
-		{
-			try {
-				GraphNode<int, int> node(5);
-				node.GetChildAtIndex(2);
-				Assert::Fail();
-			}
-			catch (std::out_of_range&)
-			{
-				Assert::IsTrue(true);
-			}
-		}
-	
-		TEST_METHOD(GraphNode_RemoveChildDefinedByDataType)
-		{
-			// pogledajte komentar za metodu GraphNode_ContainsChildReturnsFalseIfDataDoesNotMatchAnyChild
-			GraphNode<int, int> node(5, 6);
-			node.RemoveChild(6);
-			try {
-				node.GetChildAtIndex(0);
-				Assert::Fail();
-			}
-			catch (std::out_of_range&)
-			{
-				Assert::IsTrue(true);
-			}
-		}
-
-		// test pada jer GetChildAtIndex(0) vraæa nullptr i ne bace iznimku
-		TEST_METHOD(GraphNode_RemoveChildDefinedByPointer)
-		{
-			GraphNode<int, int> node1(5);
-			GraphNode<int, int>* node2=new GraphNode<int,int>(5);
-			node1.AddNewChild(node2);
-
-			node1.RemoveChild(node2);
-			try {
-				node1.GetChildAtIndex(0);
-				Assert::Fail(false);
-			}
-			catch (std::out_of_range&)
-			{
-				Assert::IsTrue(true);
-			}
-
-		}
-
-		// Test Vam je krivo napisan jer metoda ne baca iznimku. To je dobro
-		// jer metode koje "propitkuju" (i vraæaju bool) ne bi smjele bacati iznimku. 
-		// Zamislite da se želite udvarati curi i pitate je "Jesi li slobodna?" a ona Vam
-		// na to jednostavno opali šamarèinu!
-		TEST_METHOD(GraphNode_ContainsChildThrowExceptionIfNodeHasNoChildren)
-		{
-			GraphNode<int, int> node;
-			try {
-				node.ContainsChild(1);
-				Assert::Fail();
-			}
-			catch (std::out_of_range&)
-			{
-				Assert::IsTrue(true);
-			}
-		}
-
-		TEST_METHOD(GraphNode_ContainsChildReturnsTrueIfDataMatchesDataOfAtLeastOneChild)
-		{
-			// pogledajte komentar za metodu GraphNode_ContainsChildReturnsFalseIfDataDoesNotMatchAnyChild
-			GraphNode<int, int> node(5, 6);
-			Assert::IsTrue(node.ContainsChild(6));
-		}
-
-		TEST_METHOD(GraphMove_ContainsChildReturnsTrueIfAdressMatchesAddressOfAtLeastOneChild)
-		{
-			GraphNode<int, int> node1(5);
-			GraphNode<int, int>* node2 = new GraphNode<int, int>(6);
-			// Koja naredba dodaje node2 u node1?
-			Assert::IsTrue(node1.ContainsChild(node2));
-		}
-	
-		TEST_METHOD(GraphNode_ContainsChildReturnsFalseIfDataDoesNotMatchAnyChild)
-		{
-			// ovdje imate VELIKIH problema pri pozivu konstruktora 
-			// koje kompajler nije primijetio iz banalnog razloga.
-			// Imate konstruktor koji prima 3 argumenta:
-			//  template<typename dataTypeOfNode, typename moveWeightType>
-			//  GraphNode(const dataTypeOfNode& dataOfNode,
-			//	          const dataTypeOfNode& dataOfFirstChildNode,
-			//	          const moveWeightType& weightOfMovemenToNewChild = 0)
-			// i pri instantaciji kažete da su oba parametra predloška tipa int.
-			// Zbog toga su sva tri argumenta konstruktoru tipa int.
-			// Prilikom donjeg poziva u konstruktoru se dodaje novo dijete.
-			// Pritom se poziva isti konstruktor i taj rekurzivni poziv konstruktora 
-			// ide u nedogled. Ovo je jeoš jedan lijepi primjer:
-			// a) zašto ne treba nepotrebno pretrpavati suèelje (argumentom weightOfMovemenToNewChild
-			//    koji se nigdje ne koristi (ako æe Vam kasnije zatrebati, lako æete ga dodati)
-			// b) zašto nije dobro koristiti podrazumijevane vrijednosti argumenata (da
-			//    ih niste koristili, kompajler bi Vam javio pogrešku) 
-			GraphNode<int, int> node(5, 6);
-			Assert::IsFalse(node.ContainsChild(7));
-		}
-
-		TEST_METHOD(GraphNode_ContainsChildReturnsFalseIfAddressDoesNotMatchAnyChildren)
-		{
-			GraphNode<int, int> node(5, 6);
-			Assert::IsFalse(node.ContainsChild(new GraphNode<int, int>()));
 		}
 
 
